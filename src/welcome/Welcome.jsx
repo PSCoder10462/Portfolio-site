daf647fe8ce4e78f69136df76c944a23d18657b9
PSCoder10462 <pscoder10462@gmail.com>
2021-05-14 02:01:21 +0530

    welcome

--- src/welcome/Welcome.jsx
+++ src/welcome/Welcome.jsx
@@ -1,14 +1,17 @@
 import React from "react";
 import "./Welcome.css";
+import Typing from "react-typing-animation";
 
 function Welcome() {
   return (
-    <div className="welcome">
-      <h1>
-        Hello, <br /> I am
-        <span> Parv Sharma </span>
-      </h1>
-    </div>
+    <Typing>
+      <div className="welcome">
+        <h1>
+          Hello, <br /> I am
+          <span> Parv Sharma </span>
+        </h1>
+      </div>
+    </Typing>
   );
 }
 
