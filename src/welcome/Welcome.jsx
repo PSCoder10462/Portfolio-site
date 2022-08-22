bb5caea0b4eaf65c8ea45c72f44b4e7dd5df0b5a
Parv Sharma <pscoder10462@gmail.com>
2022-08-22 16:54:56 +0530

    functional resume button

--- src/welcome/Welcome.jsx
+++ src/welcome/Welcome.jsx
@@ -11,7 +11,12 @@ function Welcome() {
 					<br />
 					Web Developer, Competitive Programmer
 				</h1>
-				<button> Resume </button>
+				<a
+					href="https://docs.google.com/document/d/1VCCWKfNKj1mvoxuLTpNuSSbvqRJQIlTCHMd4aFlZm1E/edit?usp=sharing"
+					target="_blank"
+				>
+					<button> Resume </button>
+				</a>
 			</div>
 		</div>
 	);
